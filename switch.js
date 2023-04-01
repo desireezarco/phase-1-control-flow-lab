7fa4f39c04995c153c360417beffd7d0b37261fc
Desiree Zarco <desireeannzarco7@gmail.com>
2023-04-01 10:42:16 -0700

    complete lab

---
+++ switch.js
@@ -0,0 +1,16 @@
+let superheroes = "Batman";
+
+switch (superheroes){
+    case "Superman":
+        console.log("I'm flying to the rescue");
+    break;
+
+    case "Spiderman":
+        console.log("My Spidey senses are tingling");
+    break;
+
+    case "Hulk":
+        console.log("I'm going to break things!");
+    break;
+
+}
